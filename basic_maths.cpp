0dabd3bedb6e13e2e147dbb28141ad563137306d
faizanfareedkhan <yosoylilnib@gmail.com>
2026-01-31 09:07:23 +0500

    solved isPrime

--- basic_maths.cpp
+++ basic_maths.cpp
@@ -86,6 +86,17 @@ void printDivisions(int n){
 
 }
 
+void isPrime(int n){
+    int count = 0;
+    for(int i = 1 ; i*i<= n; i++){
+        if(n%i == 0){
+            count++;
+            if((n/i) != i) count++;
+        }
+    }
+    if(count == 2) cout << "true";
+    else cout << "false";
+}
 
 int main(){
     int t;
@@ -101,7 +112,8 @@ int main(){
         // cout << boolalpha << resPalindromeNumbers << endl;
         // bool resArmStrongNumbers = armStrongNumbers(n);
         // cout << boolalpha << resArmStrongNumbers << endl;
-        printDivisions(n);
+        // printDivisions(n);
+        isPrime(n);
         
     }
     
